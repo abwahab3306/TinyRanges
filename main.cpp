1a1fd7d2f8764743a457b4e6c8f3b82444b00a5e
Abdul Wahab <30478933+abwahab3306@users.noreply.github.com>
2021-06-22 23:13:36 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 /*
   Abdul Wahab Shafiq (S011313) Department	of	Computer Science
 
-    *Tiny Ranges Implementation by Furkan KIRAC
+    *Tiny Ranges Implementation by Abdul Wahab
     * as part of CS409/509 - Advanced C++ Programming course in Ozyegin University
     * Supports transforming and filtering ranges and,
     * to<CONTAINER>() method for eagerly rendering the range into a CONTAINER.*/
